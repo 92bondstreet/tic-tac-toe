d20573565d40d3c95a9d36111061318138959bf3
Yassine AZZOUT <92bondstreet@gmail.com>
2015-11-16 23:39:51 +0100

    step 5

--- lib/app.jsx
+++ lib/app.jsx
@@ -6,12 +6,25 @@ var boxStyle = {
   'height': '100px',
   'width': '100px'
 };
+var ALTERNANCE_MS = 300;
 
 var Box = React.createClass({
   'getInitialState': function onGetInitialState () {
     return {
       'value': this.props.initialValue
     };
+  },
+  'componentWillMount': function onComponentWillMount () {
+    var self = this;
+
+    this.timer = setInterval(function onTimer () {
+      self.setState({
+        'value': self.state.value === 'X' ? 'O' : 'X'
+      });
+    }, ALTERNANCE_MS);
+  },
+  'componentWillUnmount': function onComponentWillUnmount () {
+    clearInterval(this.timer);
   },
   'render': function onRender () {
     return (
