1fd00118cdae94ad0915facd5853add610585292
Yassine AZZOUT <92bondstreet@gmail.com>
2015-11-16 22:25:50 +0100

    step 1

--- lib/app.jsx
+++ lib/app.jsx
@@ -1,3 +1,17 @@
 'use strict';
 
 var React = require('react');
+
+var Box = React.createClass({
+  /**
+   * Render a HTML button
+   * @return {ReactElement}
+   */
+  'render': function onRender () {
+    return (
+      <button>X</button>
+    );
+  }
+});
+
+React.render(<Box/>, document.body);
